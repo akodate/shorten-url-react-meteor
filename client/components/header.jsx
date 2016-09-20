f5d254fbd108fa27d34bc7f0d9689bdb7520b449
akodate <alex.kodate@gmail.com>
2016-09-19 18:18:56 -0700

    Initial commit + header component

---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className='navbar-default'>
+      <div className='navbar-header'>
+        <a className='navbar-brand'>ShortenURL</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header
